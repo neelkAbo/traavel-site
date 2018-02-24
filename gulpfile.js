9a00f04cdb65ba0bab42b837f4d0b2404f987a66
neelk <aboneel1971@yahoo.com>
2018-02-24 08:29:49 +0200

    Completed large hero block and intro content block.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+
+
+gulp.task('html',function(){
+   console.log('Imagine watching htlm');
+});
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css')
+               .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+               .on('error', function(errorInfo){
+                   console.log(errorInfo.toString());
+                   this.emit('end');
+               })
+               .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+    
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
+});
+
+
